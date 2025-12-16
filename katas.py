a3544388f527f7f5323a804df07bfe93421cd2fd
palaciusjorge <palaciusjorge@gmail.com>
2025-12-16 10:01:51 +0100

    Ejercicios del 10 al 20

--- katas.py
+++ katas.py
@@ -70,4 +70,55 @@ def promedio_lista():
         return "Debes introducir solo números separados por comas"
     except ZeroDivisionError:
         return "La lista no puede estar vacía"
-    
+#Manejo de excepciones en un input
+def introducir_edad():
+    try:
+        edad = int(input("Introduce tu edad: \n"))
+        if edad < 0:
+            return "La edad no puede ser negativa"
+        elif edad > 120:
+            return "La edad no puede ser mayor de 120 años"
+        else:
+            return f"Tienes {edad} años"
+    except ValueError:
+        return "Debes introducir un número entero"
+#Uso de map para la longitud de cada palabra de la frase
+def longitud_palabras(frase):
+    lista_palabras = frase.split()
+    lista_longitudes = list(map(lambda palabra: len(palabra), lista_palabras))
+    return lista_longitudes
+#Generador de tuplas con cada caracter en minuscula y mayuscula eliminando duplicados
+def tuplas_mayus_minus(cadena):
+    lista_tuplas = list(set(map(lambda c: (c.lower(), c.upper()), cadena)))
+    return lista_tuplas
+#Uso de filter para obtener palabras empezando por una letra
+def filtrar_palabras_por_letra(lista_palabras, letra):
+    lista_filtrada = list(filter(lambda palabra: palabra.lower().startswith(letra.lower()), lista_palabras))
+    return lista_filtrada
+#Funcion lambda que suma 3 a cada elemento de la lista
+def sumar_tres_lista(lista_numeros):
+    lista_resultado = list(map(lambda x: x + 3, lista_numeros ))
+    return lista_resultado
+#Uso de filter para filtrat palabras con longitud mayor a n
+def filtrar_palabras_longitud(cadena_texto, n):
+    lista_palabras = cadena_texto.split()
+    lista_filtrada = list(filter(lambda palabra: len(palabra) >= n), lista_palabras)
+    return lista_filtrada
+#Uso de reduce para pasar de una lista de enteros a su numero concatenado
+from functools import reduce
+def concatenar_numeros(lista_numeros):
+    numero_concatenado = reduce(lambda x, y: str(x) + str(y), lista_numeros)
+    return int(numero_concatenado)
+#Uso de filter en una lista de diccionarios para filtrar por un valor
+def filtrar_diccionarios(lista_diccionarios, clave, valor):
+    lista_filtrada = list(filter(lambda diccionario: diccionario.get(clave) >= valor, lista_diccionarios))
+    return lista_filtrada
+#Uso de lambda para filtrar numeros impares en una lista
+def filtrar_numeros_impares(lista_numeros):
+    lista_impares = list(filter(lambda x: x%2 != 0, lista_numeros))
+    return lista_impares
+#Uso de filter para obtener los enteros de una lista mixta
+def filtrar_enteros(lista_mixta):
+    lista_enteros = list(filter(lambda x: isinstance(x, int), lista_mixta))
+    return lista_enteros
+
